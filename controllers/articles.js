516cd14f213ea1b0daf05f4b997743fc7da270f2
AnnaKarutina <anna.karutina@khk.ee>
2025-02-07 10:31:07 +0200

    As a user i can see single yoga article with author data. See #3.
    * As a user i can see all yoga articles. See #1
    
    * Display an article by slug
    
    * Add author name to the article

--- controllers/articles.js
+++ controllers/articles.js
@@ -14,8 +14,16 @@ const getArticlesBySlug = (req, res) => {
     const sql =`SELECT * FROM article WHERE slug="${req.params.slug}"`
     db.query(sql, (error, result) => {
         if(error) throw error;
-        res.render('article', {
-            article: result
+        const article = result[0]
+        const author_id = result[0].author_id
+        const sql =`SELECT * FROM author WHERE id="${author_id}"`
+        db.query(sql, (error, result) => {
+            if(error) throw error;
+            const author = result[0]
+            article['author_name'] = author.name
+            res.render('article', {
+                article: article
+            })
         })            
     })
 }
